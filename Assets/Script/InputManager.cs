8ff5e0befbb7fa238b493cd6a5f96a87ab5d4cad
Nolan <nolan.hubler@outlook.com>
2023-07-28 16:35:51 +0200

    Script qui permet de faire spawn des balles

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
@@ -13,10 +14,18 @@ public class InputManager : MonoBehaviour
         direction.x = Input.GetAxis("HorizontalP" + noOfPlayer);
         direction.y = Input.GetAxis("VerticalP" + noOfPlayer);
     }
+    //noOfPlayer veut dire l'un des joueurs\\
+
 
     void Update()
     {
         GetDirection();
         pc.Move(direction);
+
+        if (Input.GetButtonDown("Fire1P" + noOfPlayer))
+        {
+            pc.SpawnABalle();
+        }
     }
-}
+    }
+
--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -6,10 +6,18 @@ public class PlayerControler : MonoBehaviour
 {
     private Vector2 direction;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private GameObject Balle;
+
 
     public void Move (Vector2 moveTo)
     {
         direction = moveTo;
         rb.velocity = new Vector3(direction.x, 0, direction.y);
+    }
+
+    //Instantiate = faire apparaitre un objet\\
+    public void SpawnABalle()
+    {
+        Instantiate(Balle, transform.position + transform.forward * 2f, Quaternion.identity);
     }
 }
