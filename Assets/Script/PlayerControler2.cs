d09c01af3b0f0d67b7084e3a07e7b332682e5256
Musa Arslan <musaarslannn34@gmail.com>
2023-07-31 10:34:31 +0200

    change pas qqch

--- Assets/Script/PlayerControler2.cs
+++ Assets/Script/PlayerControler2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerControler2 : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public GameObject bulletPrefab;
@@ -11,17 +11,17 @@ public class PlayerControler : MonoBehaviour
 
     void Update()
     {
-        // Player1 için hareket kontrolleri
-        float horizontalInput = Input.GetAxis("Horizontal1");
-        float verticalInput = Input.GetAxis("Vertical1");
+        // Player2 için hareket kontrolleri
+        float horizontalInput = Input.GetAxis("Horizontal2");
+        float verticalInput = Input.GetAxis("Vertical2");
 
         Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
         Vector3 moveVelocity = moveDirection * moveSpeed;
 
         GetComponent<Rigidbody>().velocity = moveVelocity;
 
-        // Player1 için ateþ etme
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Player2 için ateþ etme
+        if (Input.GetKeyDown(KeyCode.P))
         {
             Shoot();
         }
@@ -29,7 +29,7 @@ public class PlayerControler : MonoBehaviour
 
     void Shoot()
     {
-        // Player1 için mermi oluþturma ve atýþ iþlemleri
+        // Player2 için mermi oluþturma ve atýþ iþlemleri
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
         bulletRigidbody.velocity = firePoint.forward * bulletSpeed;
